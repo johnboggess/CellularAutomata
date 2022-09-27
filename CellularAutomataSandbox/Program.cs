58c19ba5c6f5672664da3aee24cd6bcd45bc531c
John Boggess <johnboggess@jboggess.net>
2022-09-27 18:02:32 -0400

    Setup sandbox project

---
+++ CellularAutomataSandbox/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+
+namespace CellularAutomataSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Window().Run();
+        }
+    }
+}
---
+++ CellularAutomataSandbox/Window.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CellularAutomata;
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+namespace CellularAutomataSandbox
+{
+    public class Window : GameWindow
+    {
+        Simulation _simulation;
+        public Window() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            _simulation = new Simulation();
+        }
+
+        protected override void OnLoad()
+        {
+            _simulation.Load();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            _simulation.Render();
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            _simulation.Update();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            _simulation.Resize(e.Size.X, e.Size.Y);
+        }
+    }
+}
